2fab5e7b351c078cc9eb40d8b776a143b90dc31c
adaas <dmishra3@csc.com>
2013-12-30 08:22:59 +0530

    1st time checkin

---
+++ OrderByStringmethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrderByStringmethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = from m in typeof(string).GetMethods()
+                        where m.IsStatic == true
+                        orderby m.Name
+                        
+                      
+                        select new { m.Name  };
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
